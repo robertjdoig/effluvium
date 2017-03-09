320ca29be22bebce9e680791281824830ba6da31
DESKTOP-BVN8U5J\Robert Doig <robertjdoig@hotmail.co.uk>
2017-03-09 12:16:52 +0000

    Adding the white box room into the project, also been testing out shaders and pixel readers to make the simulation more effiecent

--- Effluvium_UE4/Source/Effluvium_UE4/R3D_Fluid.cpp
+++ Effluvium_UE4/Source/Effluvium_UE4/R3D_Fluid.cpp
@@ -18,6 +18,8 @@ void AR3D_Fluid::BeginPlay()
 {
   Super::BeginPlay();
 
+
+
   //dimensions = FVector(30, 30, 12);
   int size = (dimensions.X + 1)*(dimensions.Y + 1);
   for (int i = 0; i < size; i++)
@@ -30,7 +32,6 @@ void AR3D_Fluid::BeginPlay()
 
     prev_vx.Add(0.0f);
     prev_vy.Add(0.0f);
-
   }
 
   // try and fire a projectile
@@ -42,7 +43,7 @@ void AR3D_Fluid::BeginPlay()
       for (int i = 0; i < dimensions.X; i++) {
         for (int j = 0; j < dimensions.Y; j++) {
 
-          FTransform trans = FTransform(FRotator::ZeroRotator, FVector(10, 10 + (i * 20), 200 + (j * 20)));
+          FTransform trans = world_trans + FTransform(FRotator::ZeroRotator, FVector(10, 10 + (i * (object_size.X*2)), 0 + (j * (object_size.Y*2))),object_size);
           AActor* t_ref = World->SpawnActor<AActor>(ProjectileClass, trans);
           refs.Add(t_ref);
 
@@ -70,6 +71,8 @@ void AR3D_Fluid::Tick(float DeltaTime)
   float dt = 1.0f / 30;
   int N = dimensions.X - 1;
 
+  FVector dim = FVector(dimensions.X - 1, dimensions.Y - 1, dimensions.Z -1); 
+
   float* u = vx.GetData();
   float* v = vy.GetData();
   float* u_prev = prev_vx.GetData();
@@ -91,13 +94,13 @@ void AR3D_Fluid::Tick(float DeltaTime)
   float c = cos(frameCounter * 0.01f);
   float s = sin(frameCounter * 0.01f);
 
-  int x = (int)dimensions.X*0.5f, y = (int)dimensions.X * 0.5f;
-  density[IX(x, y, N)] += density_amount * dt;
-  u[IX(x, y, N)] += c * (vx_speed * dt);
-  v[IX(x, y, N)] += s * (vy_speed * dt);
+  int x = (int)dimensions.X*0.5f, y = (int)dimensions.Y * 0.5f;
+  density[IX(x, y, dim)] += density_amount * dt;
+  u[IX(x, y, dim)] += c * (vx_speed * dt);
+  v[IX(x, y, dim)] += s * (vy_speed * dt);
 
-  velocity_step(N, u, v, u_prev, v_prev, visc, dt);
-  density_step(N, dens, dens_prev, u, v, diff, dt);
+  velocity_step(dim, u, v, u_prev, v_prev, visc, dt);
+  density_step(dim, dens, dens_prev, u, v, diff, dt);
 
 
   for (int i = 0; i < dimensions.X; i++)
@@ -119,55 +122,57 @@ void AR3D_Fluid::Tick(float DeltaTime)
 }
 
 
-void AR3D_Fluid::set_boundary(int N, int b, float * x)
+void AR3D_Fluid::set_boundary(FVector dim, int b, float * x)
 {
 
-  for (int i = 1; i <= N; i++) {
-    x[IX(0, i, N)] = b == 1 ? -x[IX(1, i, N)] : x[IX(1, i, N)];
-    x[IX(N + 1, i, N)] = b == 1 ? -x[IX(N, i, N)] : x[IX(N, i, N)];
-    x[IX(i, 0, N)] = b == 2 ? -x[IX(i, 1, N)] : x[IX(i, 1, N)];
-    x[IX(i, N + 1, N)] = b == 2 ? -x[IX(i, N, N)] : x[IX(i, N, N)];
+  for (int i = 1; i <= dim.X; i++) {
+    x[IX(0, i, dim)] = b == 1 ? -x[IX(1, i, dim)] : x[IX(1, i, dim)];
+    x[IX(dim.X + 1, i, dim)] = b == 1 ? -x[IX(dim.X, i, dim)] : x[IX(dim.X, i, dim)];
+  
+  
+    x[IX(i, 0, dim)] = b == 2 ? -x[IX(i, 1, dim)] : x[IX(i, 1, dim)];
+    x[IX(i, dim.Y + 1, dim)] = b == 2 ? -x[IX(i, dim.Y, dim)] : x[IX(i, dim.Y, dim)];
   }
 
-  x[IX(0, 0, N)] = 0.5f*(x[IX(1, 0, N)] + x[IX(0, 1, N)]);
-  x[IX(0, N + 1, N)] = 0.5f*(x[IX(1, N + 1, N)] + x[IX(0, N, N)]);
-  x[IX(N + 1, 0, N)] = 0.5f*(x[IX(N, 0, N)] + x[IX(N + 1, 1, N)]);
-  x[IX(N + 1, N + 1, N)] = 0.5f*(x[IX(N, N + 1, N)] + x[IX(N + 1, N, N)]);
+  x[IX(0, 0, dim)] = 0.5f*(x[IX(1, 0, dim)] + x[IX(0, 1, dim)]);
+  x[IX(0, dim.Y + 1, dim)] = 0.5f*(x[IX(1, dim.Y + 1, dim)] + x[IX(0, dim.Y, dim)]);
+  x[IX(dim.X + 1, 0, dim)] = 0.5f*(x[IX(dim.X, 0, dim)] + x[IX(dim.X + 1, 1, dim)]);
+  x[IX(dim.X + 1, dim.Y + 1, dim)] = 0.5f*(x[IX(dim.X, dim.Y + 1, dim)] + x[IX(dim.X + 1, dim.Y, dim)]);
 
 }
 
 //lin_solve in orginal
-void AR3D_Fluid::gauss_siedel(int N, int b, float * x, float * x0, float a, float c)
+void AR3D_Fluid::gauss_siedel(FVector dim, int b, float * x, float * x0, float a, float c)
 {
 
   for (int k = 0; k<20; k++) {
-    for (int i = 1; i <= N; i++) {
-      for (int j = 1; j <= N; j++) {
-        x[IX(i, j, N)] = (x0[IX(i, j, N)] + a*(x[IX(i - 1, j, N)] + x[IX(i + 1, j, N)] + x[IX(i, j - 1, N)] + x[IX(i, j + 1, N)])) / c;
+    for (int i = 1; i <= dim.X; i++) {
+      for (int j = 1; j <= dim.Y; j++) {
+        x[IX(i, j, dim)] = (x0[IX(i, j, dim)] + a*(x[IX(i - 1, j, dim)] + x[IX(i + 1, j, dim)] + x[IX(i, j - 1, dim)] + x[IX(i, j + 1, dim)])) / c;
       }
     }
-    set_boundary(N, b, x);
+    set_boundary(dim, b, x);
   }
 }
 
-void AR3D_Fluid::diffuse(int N, int b, float * x, float * x0, float diff, float dt)
+void AR3D_Fluid::diffuse(FVector dim, int b, float * x, float * x0, float diff, float dt)
 {
-  float a = dt * diff * (N * N);
-  gauss_siedel(N, b, x, x0, a, 1 + 4 * a);
+  float a = dt * diff * (dim.X * dim.Y);
+  gauss_siedel(dim, b, x, x0, a, 1 + 4 * a);
 }
 
-void AR3D_Fluid::advect(int N, int b, float * d, float * d0, float * u, float * v, float dt)
+void AR3D_Fluid::advect(FVector dim, int b, float * d, float * d0, float * u, float * v, float dt)
 {
 
-  float dt0 = dt*N;
-  for (int i = 1; i <= N; i++) {
-    for (int j = 1; j <= N; j++) {
+  float dt0 = dt*dim.X;
+  for (int i = 1; i <= dim.X; i++) {
+    for (int j = 1; j <= dim.Y; j++) {
       // (x, y) is the address to copy from
-      float x = i - dt0*u[IX(i, j, N)], y = j - dt0*v[IX(i, j, N)];
+      float x = i - dt0*u[IX(i, j, dim)], y = j - dt0*v[IX(i, j, dim)];
 
       // clamp x and y
-      if (x < 0.5f) x = 0.5f; else if (x > N + 0.5f) x = N + 0.5f;
-      if (y < 0.5f) y = 0.5f; else if (y > N + 0.5f) y = N + 0.5f;
+      if (x < 0.5f) x = 0.5f; else if (x > dim.X + 0.5f) x = dim.X + 0.5f;
+      if (y < 0.5f) y = 0.5f; else if (y > dim.Y + 0.5f) y = dim.Y + 0.5f;
 
       // s1 and s0 are lerp coordinates [0,1) within the source cell
       int i0 = (int)x, i1 = i0 + 1;
@@ -176,87 +181,87 @@ void AR3D_Fluid::advect(int N, int b, float * d, float * d0, float * u, float * 
       float t1 = y - j0, t0 = 1 - t1;
 
       // sample the source
-      d[IX(i, j, N)] =
-        s0*(t0*d0[IX(i0, j0, N)] + t1*d0[IX(i0, j1, N)]) +
-        s1*(t0*d0[IX(i1, j0, N)] + t1*d0[IX(i1, j1, N)])
+      d[IX(i, j, dim)] =
+        s0*(t0*d0[IX(i0, j0, dim)] + t1*d0[IX(i0, j1, dim)]) +
+        s1*(t0*d0[IX(i1, j0, dim)] + t1*d0[IX(i1, j1, dim)])
         ;
     }
   }
-  set_boundary(N, b, d);
+  set_boundary(dim, b, d);
 }
 
-void AR3D_Fluid::project(int N, float * u, float * v, float * p, float * div)
+void AR3D_Fluid::project(FVector dim, float * u, float * v, float * p, float * div)
 {
   // stablisation step. adjust the velocity to prevent increase in energy
   // in the system.
 
   // calculate divergence into div
   // set initial value of p
-  for (int i = 1; i <= N; i++) {
-    for (int j = 1; j <= N; j++) {
-      div[IX(i, j, N)] = -0.5f*(u[IX(i + 1, j, N)] - u[IX(i - 1, j, N)] + v[IX(i, j + 1, N)] - v[IX(i, j - 1, N)]) / N;
-      p[IX(i, j, N)] = 0;
+  for (int i = 1; i <= dim.X; i++) {
+    for (int j = 1; j <= dim.Y; j++) {
+      div[IX(i, j, dim)] = -0.5f*(u[IX(i + 1, j, dim)] - u[IX(i - 1, j, dim)] + v[IX(i, j + 1, dim)] - v[IX(i, j - 1, dim)]) / dim.X;
+      p[IX(i, j, dim)] = 0;
     }
   }
 
   // copy pixels to boundary
-  set_boundary(N, 0, div);
-  set_boundary(N, 0, p);
+  set_boundary(dim, 0, div);
+  set_boundary(dim, 0, p);
 
   // p += div[x+/-1, y+/-1] * 4;
-  gauss_siedel(N, 0, p, div, 1, 4);
+  gauss_siedel(dim, 0, p, div, 1, 4);
 
   // calculate velocity from pressure-like "p"
-  for (int i = 1; i <= N; i++) {
-    for (int j = 1; j <= N; j++) {
+  for (int i = 1; i <= dim.X; i++) {
+    for (int j = 1; j <= dim.Y; j++) {
       // u from left and right
-      u[IX(i, j, N)] -= 0.5f*N*(p[IX(i + 1, j, N)] - p[IX(i - 1, j, N)]);
+      u[IX(i, j, dim)] -= 0.5f*dim.X*(p[IX(i + 1, j, dim)] - p[IX(i - 1, j, dim)]);
 
       // v from up and down.
-      v[IX(i, j, N)] -= 0.5f*N*(p[IX(i, j + 1, N)] - p[IX(i, j - 1, N)]);
+      v[IX(i, j, dim)] -= 0.5f*dim.Y*(p[IX(i, j + 1, dim)] - p[IX(i, j - 1, dim)]);
     }
   }
 
   // copy velocity to boundary
-  set_boundary(N, 1, u);
-  set_boundary(N, 2, v);
+  set_boundary(dim, 1, u);
+  set_boundary(dim, 2, v);
 }
 
-void AR3D_Fluid::density_step(int N, float * x, float * x0, float * u, float * v, float diff, float dt)
+void AR3D_Fluid::density_step(FVector dim, float * x, float * x0, float * u, float * v, float diff, float dt)
 {
   // apply diffusion to density. If there is no velocity, the value will still spread.  
   Swap(x0, x);
   //fSwap(x0, x);
-  diffuse(N, 0, x, x0, diff, dt);
+  diffuse(dim, 0, x, x0, diff, dt);
 
   // use the velocity field to carry density around.
   Swap(x0, x);
   //fSwap(x0, x);
-  advect(N, 0, x, x0, u, v, dt);
+  advect(dim, 0, x, x0, u, v, dt);
 }
 
-void AR3D_Fluid::velocity_step(int N, float * u, float * v, float * u0, float * v0, float visc, float dt)
+void AR3D_Fluid::velocity_step(FVector dim, float * u, float * v, float * u0, float * v0, float visc, float dt)
 {
   /// Compute the new velocity field.
   // diffuse into neighouring cells
   Swap(u0, u);
-  diffuse(N, 1, u, u0, visc, dt);
+  diffuse(dim, 1, u, u0, visc, dt);
   Swap(v0, v);
-  diffuse(N, 2, v, v0, visc, dt);
+  diffuse(dim, 2, v, v0, visc, dt);
 
   // stabilise the system using poisson
-  project(N, u, v, u0, v0);
+  project(dim, u, v, u0, v0);
   Swap(u0, u);
   Swap(v0, v);
 
   // use advection to move the velocity itself
-  advect(N, 1, u, u0, u0, v0, dt);
-  advect(N, 2, v, v0, u0, v0, dt);
+  advect(dim, 1, u, u0, u0, v0, dt);
+  advect(dim, 2, v, v0, u0, v0, dt);
 
   // stabilise the system using poisson
-  project(N, u, v, u0, v0);
+  project(dim, u, v, u0, v0);
 }
 
-int AR3D_Fluid::IX(int i, int j, int N) {
-  return ((i)+(N + 2) * (j));
+int AR3D_Fluid::IX(int i, int j, FVector dim) {
+  return (i)+((dim.X + 2) * (j));
 }
--- Effluvium_UE4/Source/Effluvium_UE4/R3D_Fluid.h
+++ Effluvium_UE4/Source/Effluvium_UE4/R3D_Fluid.h
@@ -20,6 +20,8 @@ public:
 	// Called every frame
 	virtual void Tick( float DeltaSeconds ) override;
 
+
+
   /** Projectile class to spawn */
   UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Array_Ob)
     TSubclassOf<class AActor> ProjectileClass;
@@ -31,7 +33,13 @@ public:
     TArray<UMaterialInstanceDynamic*> materials;
 
   UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Array_Ob)
+    FTransform world_trans;
+
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Array_Ob)
     FVector dimensions;
+
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Array_Ob)
+    FVector object_size;
 
   UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Array_Ob)
     TArray<float> density;
@@ -47,15 +55,15 @@ public:
   UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Array_Ob)
     TArray<float> prev_vy;
 
-  void set_boundary(int N, int b, float * x);
-  void gauss_siedel(int N, int b, float * x, float * x0, float a, float c);
-  void diffuse(int N, int b, float * x, float * x0, float diff, float dt);
-  void advect(int N, int b, float * d, float * d0, float * u, float * v, float dt);
-  void project(int N, float * u, float * v, float * p, float * div);
-  void density_step(int N, float * x, float * x0, float * u, float * v, float diff, float dt);
-  void velocity_step(int N, float * u, float * v, float * u0, float * v0, float visc, float dt);
+  void set_boundary(FVector dim, int b, float * x);
+  void gauss_siedel(FVector dim, int b, float * x, float * x0, float a, float c);
+  void diffuse(FVector dim, int b, float * x, float * x0, float diff, float dt);
+  void advect(FVector dim, int b, float * d, float * d0, float * u, float * v, float dt);
+  void project(FVector dim, float * u, float * v, float * p, float * div);
+  void density_step(FVector dim, float * x, float * x0, float * u, float * v, float diff, float dt);
+  void velocity_step(FVector dim, float * u, float * v, float * u0, float * v0, float visc, float dt);
 
-  int IX(int i, int j, int N);
+  int IX(int i, int j, FVector dim);
 	
   /** UI TESTING */
   UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Testing)
